1a59884df8c74c667efeea0d396024023557e195
JoelDiaz93 <58042087+JoelDiaz93@users.noreply.github.com>
2020-08-03 09:13:59 -0500

    Add files via upload

---
+++ sudoku/src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+//JUEGO EDITADO (PARA PRESENTAR) 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        UserInterfaces userInterface = new UserInterfaces(scanner);
+        userInterface.start();
+    }
+
+}
+
